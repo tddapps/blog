1989ae4f5499caa663fb7527a01d6837793ca6f2
camilin87 <camilin87@gmail.com>
2015-08-19 06:39:22 -0400

    start automating the theme installation

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,8 @@
 task :deploy do
     sh "git push origin gh-pages"
 end
+
+task :install_theme, [:theme_folder] do |t, args|
+    src_path = args[:theme_folder]
+    puts "Installing theme from #{src_path}"
+end
