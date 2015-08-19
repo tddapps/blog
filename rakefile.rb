2e3822346b7e8f669bcc895343b6de79f8fd5a6c
camilin87 <camilin87@gmail.com>
2015-08-19 06:53:08 -0400

    automate theme installation... as best as I could

--- rakefile.rb
+++ rakefile.rb
@@ -5,4 +5,15 @@ end
 task :install_theme, [:theme_folder] do |t, args|
     src_path = args[:theme_folder]
     puts "Installing theme from #{src_path}"
+
+    FileList
+        .new("*")
+        .exclude("_posts", "CNAME", "readme.md", "rakefile.rb")
+        .each { |f|
+            sh "rm -R -v #{f}"
+        }
+
+    sh "mv readme.md readme.md.bck"
+    sh "cp -f -v -R #{src_path}/* ./"
+    sh "mv readme.md.bck readme.md"
 end
--- readme.md
+++ readme.md
@@ -4,14 +4,10 @@
 
 `rake deploy`
 
-
 ##Development
 
 `jekyll serve` Loads the page locally on http://localhost:4000/
 
 ###Changing Theme
 
-    rm -R -v !(_posts|CNAME|readme.md|rakefile.rb)
-    mv readme.md readme.md.bck
-    cp -f -v -R YOUR_THEME_FOLDER/* ./
-    mv readme.md.bck readme.md
+`rake install_theme[~/Downloads/pixyll/]`
